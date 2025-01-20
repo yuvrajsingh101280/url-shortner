2ec12b64a219f102ed7c6b2a5ef012a72a3dadb2
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-01-20 11:14:53 +0530

    remove unused React import in DataTable and Container components

--- frontned/src/components/DataTable/DataTable.tsx
+++ frontned/src/components/DataTable/DataTable.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { urlData } from "../../interface/urlData";
 import { Link } from "react-router-dom";
 import axios from "axios";
--- frontned/src/components/container/Container.tsx
+++ frontned/src/components/container/Container.tsx
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import FormContainer from "../FormContainer/FormContainer";
 import { urlData } from "../../interface/urlData";
 import axios from "axios";
 import DataTable from "../DataTable/DataTable";
 // import server_url from "../../../helpers/server_url.js";
-type Props = {};
 
-const Container = (props: Props) => {
+const Container = () => {
   const [data, setData] = useState<urlData[]>([]);
   const [reload, setReload] = useState<boolean>(false);
   const updateReloadState = (): void => {
