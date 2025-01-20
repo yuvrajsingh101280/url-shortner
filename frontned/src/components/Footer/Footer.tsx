21646a4f9a507fdcec61f7416bfec23351a37b79
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2025-01-20 11:16:11 +0530

    simplify Footer component by removing unused props and React import

--- frontned/src/components/Footer/Footer.tsx
+++ frontned/src/components/Footer/Footer.tsx
@@ -1,8 +1,4 @@
-import React from "react";
-
-type Props = {};
-
-const Footer = (props: Props) => {
+const Footer = () => {
   return (
     <div className="bg-slate-900 text-base text-white text-center py-5">
       Copyright &#169; URLShortner | Yuvraj singh
